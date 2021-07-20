ec81015cc75887ff2bb9d51fdf690eb193f472d8
Laisvunas <saugos@gmail.com>
2021-07-20 11:58:51 +0300

    Registered Textarea component

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Input } from "./Input/Input";
 export { default as Notification } from "./Notification/Notification";
 export { default as ProgressBar } from "./ProgressBar/ProgressBar";
 export { default as Navigation } from "./Navigation/Navigation";
+export { default as Textarea } from "./Textarea/Textarea";
