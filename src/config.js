660f89ca8ca36affc08b0547befa3dc676bcd7b1
Laisvunas <saugos@gmail.com>
2021-07-20 09:35:38 +0300

    added site name to config

--- src/config.js
+++ src/config.js
@@ -2,4 +2,6 @@ module.exports = {
     //backendUrlBase: 'https://team-ratings-back-5r9sc.ondigitalocean.app'
   
     backendUrlBase: 'http://localhost:8080',
+
+    siteName: 'RKM Diagrammer',
   };
