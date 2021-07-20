67783db41de441e00d217bb9c4afec3ae4d69dae
Laisvunas <saugos@gmail.com>
2021-07-20 09:36:29 +0300

    removed unneded import

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Routes from "./Routes";
+import Routes from "./routes/Routes";
+import AuthProvider from "./contexts/authContext";
 
 ReactDOM.render(
     <React.StrictMode>
-      <Routes />
+
+        <Routes />
+
     </React.StrictMode>,
     document.getElementById("root")
 );
