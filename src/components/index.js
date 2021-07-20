b46ab95564f83c0913d264a729a6bb4149b02267
Laisvunas <saugos@gmail.com>
2021-07-20 09:39:02 +0300

    created file to export all components

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Button } from "./Button/Button";
+export { default as Container } from "./Container/Container";
+export { default as Input } from "./Input/Input";
+export { default as Notification } from "./Notification/Notification";
+export { default as ProgressBar } from "./ProgressBar/ProgressBar";
+export { default as Navigation } from "./Navigation/Navigation";
