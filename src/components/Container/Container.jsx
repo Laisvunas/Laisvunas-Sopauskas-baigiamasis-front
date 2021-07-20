2eb3b9e88c647adbf6cc642118adc00183133a0b
Laisvunas <saugos@gmail.com>
2021-07-20 09:40:28 +0300

    Container component created

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import 'bulma/css/bulma.css';
+import * as S from "./Container.style";
+
+function Container(props) {
+    const { variant, children } = props;
+
+    if (variant === "wide") {
+        return (
+            <S.WideDiv>{children}</S.WideDiv>
+        );
+    }
+    else if (variant === "padded") {
+        return (
+            <S.PaddedDiv>{children}</S.PaddedDiv>
+        );
+    }
+
+    return (
+        <S.Div>{children}</S.Div>
+    );
+};
+
+Container.propTypes = {
+    onClick: PropTypes.func,
+};
+
+Container.defaultProps = {
+    onClick: undefined,
+};
+
+export default Container;
---
+++ src/components/Container/Container.style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Div = styled.div`
+max-width: 768px;
+margin: 1.5rem auto 1.5rem auto;
+`;
+
+export const WideDiv = styled.div`
+max-width: 960px;
+margin: 0 auto;
+`;
+
+export const PaddedDiv = styled.div`
+margin: 1.5rem 0 1.5rem 0;
+`;
