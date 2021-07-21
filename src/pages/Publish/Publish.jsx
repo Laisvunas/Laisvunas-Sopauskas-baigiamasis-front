e7ba3c83f223656c72192482b50860a5e31d5a51
Laisvunas <saugos@gmail.com>
2021-07-21 09:17:46 +0300

    added publishDiagram function

--- src/pages/Publish/Publish.jsx
+++ src/pages/Publish/Publish.jsx
@@ -1,21 +1,84 @@
 import React, { useContext, useState, useEffect } from "react";
+import { useParams } from 'react-router-dom';
 import { Container, Navigation, Input, Textarea, Button, Notification } from "../../components";
 
 const { backendUrlBase, siteName } = require('../../config');
 
 const Publish = (props) => {
+    const [diagramId, setId] = useState();
     const [title, setTitle] = useState();
     const [sentence, setSentence] = useState();
     const [code, setCode] = useState();
     const [commentary, setCommentary] = useState();
     const [editorCommentary, setEditorCommentary] = useState();
 
+    const { id } = useParams();
+
     useEffect(() => {
-        document.title = `Publish | ${siteName}`
+        document.title = `Publish | ${siteName}`;
+        
+        if (typeof id !== 'undefined') {
+            setId(id);
+            console.log("1 id: " + id);
+        }
+           
     }, []);
 
-    const publishDiagram = () => {
+    console.log("props.auth: " + JSON.stringify(props.auth));
 
+    const publishDiagram = () => {
+        const url = `${backendUrlBase}/diagrams/publish`;
+        const token = localStorage.getItem("token");
+
+        console.log("props: " + JSON.stringify(props));
+        console.log("2 id: " + id);
+
+        try {
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                  authorization: `Bearer ${token}`,
+                },
+                body: JSON.stringify({
+                    id: diagramId,
+                    author_id: props.auth.author_id,
+                    title: title,
+                    sentence: sentence,
+                    code: code,
+                    commentary: commentary,
+                    editors_commentary: editorCommentary,
+                    editor: props.auth.isEditor,
+                }),
+            })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log("data: " + JSON.stringify(data));
+                if (typeof data.msg !== 'undefined') {
+                    document.querySelector('.notification.is-success > div.notification-body').innerText = data.msg;
+                    document.querySelector('.notification.is-success').classList.remove('is-hidden');
+                    document.querySelector('.notification.is-success').style.display = 'block';
+                    if (typeof data.data !== 'undefined' && typeof data.data.insertId !== 'undefined' && data.data.insertId !== 0) {
+                        setId(data.data.insertId);
+                        window.history.replaceState(null, null, `/publish/${data.data.insertId}`);
+                        document.title = `Edit | ${siteName}`;
+                        document.querySelector("#page-title").innerText = "Edit Diagram";
+                    }
+                }
+                else if (typeof data.error !== 'undefined') {
+                    // console.log(data.error);
+                    document.querySelector('.notification.is-danger > div.notification-body').innerText = 'Some error happened. Try again later.';
+                    document.querySelector('.notification.is-danger').classList.remove('is-hidden');
+                    document.querySelector('.notification.is-danger').style.display = 'block';
+                }
+
+            });
+        }
+        catch (e) {
+            document.querySelector('.notification.is-danger > div.notification-body').innerText = 'Some error happened. Try again later.';
+            document.querySelector('.notification.is-danger').classList.remove('is-hidden');
+            document.querySelector('.notification.is-danger').style.display = 'block';
+        }
     };
 
     return (
@@ -26,7 +89,7 @@ const Publish = (props) => {
                 </aside>
                 <div className="column is-9 messages hero is-fullheight">
                     <h1 className="title is-1">{siteName}</h1>
-                    <h2 className="title is-2">Publish Diagram</h2>
+                    <h2 id="page-title" className="title is-2">Publish Diagram</h2>
                     <Notification variant='is-success is-hidden' />
                     <Notification variant='is-danger is-hidden' />
                     <div id="publish-diagram">
