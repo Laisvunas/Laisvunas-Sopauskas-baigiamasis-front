27f0dd18d776096829100fb207d0af9dac7f22a4
Laisvunas <saugos@gmail.com>
2021-07-25 18:08:21 +0300

    NotFound route added

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,21 +2,21 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { PrivateRoute } from "./PrivateRoute";
 
-import { Home, Login, Register, Diagrams, Publish, View, Generate } from "../pages";
+import { Home, Login, Register, Diagrams, Publish, View, Generate, NotFound } from "../pages";
 
 const Routes = () => (
   <Router>
     <Switch>
       <Route exact path="/" component={Home} />
-      <PrivateRoute exact path="/generate" component={Generate} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/register" component={Register} />
+      <PrivateRoute exact path="/generate" component={Generate} />
       <PrivateRoute exact path="/diagrams" component={Diagrams} />
       <PrivateRoute exact path="/publish/" component={Publish} />
       <PrivateRoute exact path="/publish/:id" component={Publish} />
       <PrivateRoute exact path="/view/" component={View} />
       <PrivateRoute exact path="/view/:id" component={View} />
-      <Route path='*' exact={true} component={Login} />
+      <Route path='*' exact={true} component={NotFound} />
     </Switch>
   </Router>
 );
