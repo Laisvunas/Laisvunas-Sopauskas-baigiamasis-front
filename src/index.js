1e5e3850618dad6b9ee6b257dac8f105992f7088
Laisvunas <saugos@gmail.com>
2021-07-25 17:32:16 +0300

    routes corrected

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import FontStyles from "./fonts/fontStyles";
 import Routes from "./routes/Routes";
-import AuthProvider from "./contexts/authContext";
 import './index.css';
 
 ReactDOM.render(
     <React.StrictMode>
         <FontStyles />
         <Routes />
-
     </React.StrictMode>,
     document.getElementById("root")
 );
