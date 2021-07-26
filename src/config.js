14f1c2cee83ef76a22f7cccf0b602f905e359f33
Laisvunas <saugos@gmail.com>
2021-07-26 13:47:12 +0300

    config corrected

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     //backendUrlBase: 'https://team-ratings-back-5r9sc.ondigitalocean.app'
   
-    backendUrlBase: 'https://mrk-diagrammer-ic477.ondigitalocean.app/',
+    backendUrlBase: 'https://mrk-diagrammer-ic477.ondigitalocean.app',
 
     siteName: 'MRK Diagrammer',
   };
